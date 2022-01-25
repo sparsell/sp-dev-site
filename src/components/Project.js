316bae288535c746ea3b8a0316ba63c55afe3741
sparsell <hello@sparsell.com>
2022-01-25 12:49:36 -0500

    basic structure for each component

---
+++ src/components/Project.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Project extends React.Component {
+    render() {
+    return(
+        <div>
+            <h2> First Project </h2>
+            <p> Details </p>
+        </div>
+    )
+    }
+}
+
+export default Project
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,1 +1,14 @@
-// import React from 'react'
+import React from 'react'
+import Project from './Project'
+
+const Projects = () => {
+    return(
+        <div>
+            <Project />
+            <Project />
+            <Project />
+        </div>
+    )
+}
+
+export default Projects
