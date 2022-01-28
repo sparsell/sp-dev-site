c96794acbe23c6b67e8e9a734a3440cd1bb07f39
sparsell <hello@sparsell.com>
2022-01-28 14:41:18 -0500

    started adding projects

---
+++ src/projectdata.js
@@ -0,0 +1,23 @@
+export const projects = [
+    {   
+        title: "ClassTech",
+        description: "Loreum ipsum", 
+        image: "./project-1.gif",
+        link: "coming soon",
+        github: "https://github.com/sparsell/classtech-frontend"
+    },
+    {   
+        title: "SpendLess",
+        description: "Loreum ipsum", 
+        image: "./project-2.gif",
+        link: "coming soon",
+        github: "https://github.com/sparsell/classtech-frontend"
+    },
+    {   
+        title: "Unpaid Work",
+        description: "Loreum ipsum", 
+        image: "./project-3.gif",
+        link: "coming soon",
+        github: "https://github.com/sparsell/classtech-frontend"
+    }
+]
