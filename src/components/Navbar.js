6acbb0e8aace9187cef5949624e84da064235967
sparsell <hello@sparsell.com>
2022-02-02 10:30:15 -0500

    wip navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,26 @@ const Navbar = () => {
     return (
         <header classNmae="bg-gray-800 md:sticky top-0 z-10">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
-            About
-          </a>
-        </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
-            Work
-          </a>
+            <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+        {/* <a className="title-font font-medium text-white mb-4 md:mb-0"> */}
+          
+        {/* </a> */}
+        
         </nav>
         <a
+          href="#about"
+          className="ml-3 text-xl">
+          About
+        </a>
+        <a
+          href="#projects "
+          className="ml-3 text-xl">
+          Work
+        </a>
+        <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-          Hire Me
+          className="ml-3 text-xl">
+          Contact
         </a>
       </div>
     </header>
