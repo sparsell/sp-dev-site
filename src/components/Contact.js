f2ae26b1acce4b2e662851c974d56481df5611ba
sparsell <hello@sparsell.com>
2022-02-02 10:12:50 -0500

    removing fontawesome for now

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Resume from './Resume'
 
 const Contact = () => {
     return (
@@ -10,73 +11,28 @@ const Contact = () => {
 
                     <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                         EMAIL
-                        </h2>
-            
+                         EMAIL</h2>
                             <p className="text-indigo-400 leading-relaxed">
-                            <FontAwesomeIcon icon="check-square" /> <a href="mailto:hello@sparsell.com">hello@sparsell.com</a>
-                        </p>
+                                <a href="mailto:hello@sparsell.com">
+                            hello@sparsell.com</a></p>
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                         PHONE
-                        </h2>
-                        <FontAwesomeIcon icon="fa-solid fa-phone" />
-                        
-                            <p className="text-indigo-400 leading-relaxed"><FontAwesomeIcon icon="check-square" />(203) 321-9889</p>
+                         PHONE</h2>
+                            <p className="text-indigo-400 leading-relaxed">
+                            (203) 321-9889</p>
 
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                             SOCIAL
-                        </h2>
-                            {/* <p className="text-indigo-400 leading-relaxed"></p> */}
-                             <p ><FontAwesomeIcon icon="check-square" /> [linkedin]</p>
-                             <p><FontAwesomeIcon icon="check-square" /> [twitter]</p>
-                             <p><FontAwesomeIcon icon="check-square" /> [github]</p>
-                             <p><FontAwesomeIcon icon="check-square" /> [dev.to]</p>
+                         SOCIAL</h2>
+                            <p className="text-indigo-400 leading-relaxed"><a href="https://www.linkedin.com/in/sonja-parsell/" target="_blank">
+                                LinkedIn</a></p>
+                            <p className="text-indigo-400 leading-relaxed"><a href="https://twitter.com/s_parsell" target="_blank">
+                                Twitter</a></p>
+                            <p className="text-indigo-400 leading-relaxed"><a href="https://github.com/sparsell" target="_blank">
+                                GitHub</a></p>
 
                          <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                         CV
-                        </h2> 
-{/* 
-                        <form>
-                            <div className="relative mb-4">
-                                <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-                                     Name
-                                </label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                                 />
-                            </div>
-                            <div className="relative mb-4">
-                                <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-                                Email
-                                </label>
-                                <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                                />
-                            </div>
-                            <div className="relative mb-4">
-                                <label
-                                 htmlFor="message"
-                                className="leading-7 text-sm text-gray-400">
-                                Message
-                                </label>
-                                <textarea
-                                id="message"
-                                name="message"
-                                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2                   focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6                 transition-colors duration-200 ease-in-out"
-                                />
-                            </div>
-                            <button
-                                type="submit"
-                                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                                Submit
-                            </button>
-                        </form> */}
+                         CV</h2> 
+                         <Resume />         
+                        
                     </div>
 
                 </div>
