19e0cb30d3182a950276aa34fe1207bbe08446a8
sparsell <hello@sparsell.com>
2022-02-04 14:38:37 -0500

    added skills

--- src/skills.js
+++ src/skills.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 export const skills = [
 
-    "JavaScript", "React", "Redux", "Ruby", "Ruby on Rails", "HTML", "CSS", "PostgreSQL", "SQLite"
+    "JavaScript", 
+    "React", 
+    "Redux", 
+    "Ruby", 
+    "Ruby on Rails", 
+    "HTML", 
+    "CSS", 
+    "PostgreSQL", 
+    "SQLite", 
+    "MVC design patterns", 
+    "RESTful routing and APIs"
 
 ]
 
