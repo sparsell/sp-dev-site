b8622735d279c245b5e18d5d6c70b8b120467562
sparsell <hello@sparsell.com>
2022-02-04 14:40:13 -0500

    cleaned up spacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,31 +2,31 @@ import React from 'react'
 
 const Navbar = () => {
     return (
+      <section id="navbar">
         <header classNmae="bg-stone-400 md:sticky top-0 z-10">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-        {/* <a className="title-font font-medium text-white mb-4 md:mb-0"> */}
-          
-        {/* </a> */}
-        
-        </nav>
-        <a
-          href="#about"
-          className="ml-3 text-xl">
-          About
-        </a>
-        <a
-          href="#projects "
-          className="ml-3 text-xl">
-          Work
-        </a>
-        <a
-          href="#contact"
-          className="ml-3 text-xl">
-          Contact
-        </a>
-      </div>
-    </header>
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center md:sticky top-0 z-10">
+              <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center md:sticky top-0 z-10"></nav>
+                <a
+                href="#about"
+                className="ml-3 text-xl">
+                ABOUT
+                </a>
+
+                <a
+                href="#projects "
+                className="ml-3 text-xl">
+                WORK
+                </a>
+
+                <a
+                href="#contact"
+                className="ml-3 text-xl">
+                CONTACT
+                </a>
+              
+            </div>
+        </header>
+    </section>
     )
 }
 
