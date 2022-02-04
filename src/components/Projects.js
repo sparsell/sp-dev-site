1e61dc413b5eef0b96ff664b3d64eaba38adbae4
sparsell <hello@sparsell.com>
2022-02-04 14:40:41 -0500

    updated copy and h1, bg colors

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,14 +4,14 @@ import { projects } from "../projectdata"
 const Projects = () => {
     return(
         <section id="projects">
-            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+            <div className="container bg-stone-400 mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-red-50">
                         W O R K
                         </h1>
 
                         <p>
-                        From the projects I created while completing coursework and self-study, through to the side projects I work on now, I always strive to solve my own problems. The applications highlighted below were all important to me, which made the work, and more importantly the learning, all the more fulfilling.
+                        I always strive to solve my own problems when it comes to what projects to build when learning something new. The applications showcased here were all important to me, which made the work, and more importantly the learning, fulfilling.
                         </p>
                 </div>
 
