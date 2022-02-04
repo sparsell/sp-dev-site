9ae8ec465e34e19e94b7cca6ce8fd4a21f32c3c4
sparsell <hello@sparsell.com>
2022-02-04 12:27:48 -0500

    color change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <header classNmae="bg-gray-800 md:sticky top-0 z-10">
+        <header classNmae="bg-stone-400 md:sticky top-0 z-10">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
             <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
         {/* <a className="title-font font-medium text-white mb-4 md:mb-0"> */}
