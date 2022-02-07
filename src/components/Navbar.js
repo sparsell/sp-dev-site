3fa609c7c3dda5d0489df589d587cf5ff6c0b4bf
sparsell <hello@sparsell.com>
2022-02-07 18:02:59 -0500

    changed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,32 @@ import React from 'react'
 const Navbar = () => {
     return (
       <section id="navbar">
-        <header className="md:sticky top-0 z-10">
+        <nav class="p-2 mt-0 fixed w-full z-10 top-0">
+          <div class="container mx-auto flex flex-wrap items-center">
+            <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
+                {/* <a class="text-white no-underline hover:text-white hover:no-underline" href="#">
+                    <span class="text-2xl pl-2"><i class="em em-grinning"></i> Home</span>
+                </a> */}
+            </div>
+            <div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
+                <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
+                  <li class="mr-3">
+                  <a class="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#">HOME</a>
+                  </li>
+                  <li class="mr-3">
+                    <a class="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#projects">WORK</a>
+                  </li>
+                  <li class="mr-3">
+                    <a class="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#about">ABOUT</a>
+                  </li>
+                    <li class="mr-3">
+                    <a class="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#contact">CONTACT</a>
+                  </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        {/* <header className="md:sticky top-0 z-10">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
               <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center"></nav>
                 <a
@@ -25,7 +50,8 @@ const Navbar = () => {
                 </a>
               
             </div>
-        </header>
+        </header> */}
+
     </section>
     )
 }
