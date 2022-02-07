793a8217794c9d05b7c476c08fd30248352db983
sparsell <hello@sparsell.com>
2022-02-07 15:32:08 -0500

    added media queries for screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     colors: {
       'blue': '#1fb6ff',
       'purple': '#7e5bef',
