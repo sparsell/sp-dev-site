258048cf454215285278c4d4770fa915d7ef6a9c
sparsell <hello@sparsell.com>
2022-02-07 18:00:44 -0500

    edited career changer copy

--- src/components/About.js
+++ src/components/About.js
@@ -8,16 +8,15 @@ const About = () => {
            
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-red-50">A B O U T</h1>
-                    <img
-                        className="object-cover object-center rounded max-w-sm"
-                        alt="Sonja Parsell"
-                        src="../headshot.jpg"
-                    />
-                        <h1>Me before code...</h1>
-                            <p className="text-blue-800">After 15+ years at various Wall Street banks, I handed in my spreadsheets for the chance to stay at home with my two children. In my last role, I'd made a jump from Finance to Technology and after a very short time, I felt like I'd been missing out on my true calling - just as I was about to leave on what turned out to be a ten year career break.</p>
+                        <img
+                            className="object-cover object-center rounded max-w-sm"
+                            alt="Sonja Parsell"
+                            src="../headshot.jpg"/>
+                    <h1>ME BEFORE CODE...</h1>
+                        <p className="text-blue-800">After 15+ years at various Wall Street banks, I handed inmy spreadsheets for the chance to stay at home with my two children. In my last role,I'd made a jump from Finance to Technology and after a very short time, I felt like I'dbeen missing out on my true calling - just as I was about to leave on what turned out tobe a ten year career break.</p>
                             
-                        <h1>And after...</h1>
-                            <p className="text-blue-800">Now I'm passionate about blockchain technology and finding ways to add value by combining my financial services expertise with my newly minted software engineering skills. </p>
+                    <h1>AND AFTER...</h1>
+                        <p className="text-blue-800">Now I'm passionate about blockchain technology and findingways to add value by combining my financial services expertise with my newly mintedsoftware engineering skills. </p>
                 </div>
             </div>
         </section>
