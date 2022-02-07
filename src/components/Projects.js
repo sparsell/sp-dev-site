bfa3a6eafb72362c6c1c66e709e05fdffddca9a1
sparsell <hello@sparsell.com>
2022-02-07 18:03:46 -0500

    wip: getting github link to work

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,22 +9,24 @@ const Projects = () => {
             <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-red-50">
                W O R K
             </h1>
-
+              <div>
                 <p className="text-blue-800">
-                I always strive to solve my own problems when it comes to what projects to build to help me learn something new. The applications showcased here were all important to me, which made the work, and more importantly the learning, fulfilling.
+                I always strive to solve my own problems when it comes to building something to help me learn a new language or framework.<p/>
+                <p className="text-blue-800 pt-3">The applications showcased here were all important to me, which made the work, and more importantly the learning, fulfilling.</p>
                 </p>
+              </div>
           </div>
 
           <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
             {projects.map((project) => (
-              <div key={project} className="p-2 sm:w-1/2 w-full">
+              <div key={project.id} className="p-2 sm:w-1/2 w-full">
                 <div className="bg-red-100 rounded flex p-4 h-full items-center">
                   <span className="title-font font-medium text-white">
-                    <p className="text-blue-800 font-medium">{project.title}</p>
+                    <p className="text-blue-800 font-semibold tracking-widest underline underline-offset-4">{project.title}</p>
                     <p className="text-blue-800">{project.description}</p>
                     <p className="text-blue-800">{project.image}</p>
                     <p className="text-blue-800">{project.link}</p>               
-                    <p className="text-blue-800"><a target="_blank" alt="Github repo link" href="{project.github}">GITHUB</a></p>               
+                    <p className="text-blue-800 visited:text-purple-600"><a target="_blank" alt="Github repo link" href="{project.github}">GITHUB repo</a></p>         
                   </span>
                 </div>
               </div>
