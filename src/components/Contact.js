24cf167c7606b718d016eb6527f6860cc927064c
sparsell <hello@sparsell.com>
2022-02-07 18:01:38 -0500

    removed background and changed text color

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,26 @@
 import React from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Resume from './Resume'
+import SonjaParsellResume from '../assets/SonjaParsellResume.pdf'
 
 const Contact = () => {
     return (
         <section id="contact">
-            <div className="container mx-auto flex bg-red-50 px-10 py-20 md:flex-row flex-col items-center">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-RED-50">C O N T A C T</h1>
                     
-                    <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                        <h2 className="title-font font-semibold text-blue-800 bg-red-100 bg-contain tracking-widest text-xs mt-4">
+                    <div className="lg:w-1/2 px-6 lg:mt-0">
+                        <h2 className="title-font font-semibold text-blue-800 tracking-widest text-xs mt-4">
                          EMAIL</h2>
                             <p className="text-indigo-800 leading-relaxed">
                                 <a href="mailto:hello@sparsell.com">
                             hello@sparsell.com</a></p>
-                        <h2 className="title-font font-semibold text-blue-800 bg-red-100 bg-contain tracking-widest text-xs mt-4">
+                        <h2 className="title-font font-semibold text-blue-800 tracking-widest text-xs mt-4">
                          PHONE</h2>
-                            <p className="text-indigo-800 leading-relaxed">
+                            <p className="text-blue-800 leading-relaxed">
                             (203) 321-9889</p>
 
                         <h2 className="title-font font-semibold text-blue-800 tracking-widest text-xs mt-4">
-                         SOCIAL</h2>
+                            SOCIAL </h2>
                             <p className="text-blue-700 leading-relaxed"><a href="https://www.linkedin.com/in/sonja-parsell/" target="_blank">
                                 LinkedIn</a></p>
                             <p className="text-indigo-800 leading-relaxed"><a href="https://twitter.com/s_parsell" target="_blank">
@@ -29,12 +28,13 @@ const Contact = () => {
                             <p className="text-indigo-800 leading-relaxed"><a href="https://github.com/sparsell" target="_blank">
                                 GitHub</a></p>
 
-                         <h2 className="title-font font-semibold text-blue-800 bg-red-100 bg-contain tracking-widest text-xs mt-4">
-                         CV</h2> 
-                         <Resume />         
-                        
+                        <h2 className="title-font font-semibold text-blue-800 tracking-widest text-xs mt-4">
+                            CV </h2> 
+                         <a className="text-red-400 text-xs " href={SonjaParsellResume} target="_blank">
+                                <button label="Resume">
+                                    Download 
+                                </button></a>        
                     </div>
-
                 </div>
             </div>    
         </section>
