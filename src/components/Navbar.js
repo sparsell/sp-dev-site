742b9a3c2a497c677bd71261f877657b404a4489
sparsell <hello@sparsell.com>
2022-02-07 15:41:54 -0500

    edited order of items and color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,24 +3,24 @@ import React from 'react'
 const Navbar = () => {
     return (
       <section id="navbar">
-        <header classNmae="bg-stone-400 md:sticky top-0 z-10">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center md:sticky top-0 z-10">
-              <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center md:sticky top-0 z-10"></nav>
+        <header className="md:sticky top-0 z-10">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+              <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center"></nav>
                 <a
-                href="#about"
-                className="ml-3 text-xl">
-                ABOUT
-                </a>
-
-                <a
-                href="#projects "
-                className="ml-3 text-xl">
+                href="#projects"
+                className="ml-3 bg-red-100 text-md text-blue-800 pl-1 pr-1">
                 WORK
                 </a>
 
                 <a
+                href="#about"
+                className="ml-3 bg-red-100 text-md text-blue-800 pl-1 pr-1">
+                ABOUT
+                </a>
+
+                <a
                 href="#contact"
-                className="ml-3 text-xl">
+                className="ml-3 bg-red-100 text-md text-blue-800 pl-1 pr-1">
                 CONTACT
                 </a>
               
