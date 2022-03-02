87ebbafc3e96851492d5b93f579267a09c2e3f24
sparsell <hello@sparsell.com>
2022-03-02 18:20:09 -0500

    styled nav bar - sticky

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Intro from './components/Intro'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+
 import './App.css';
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
+      <Navbar sticky="top"/>
       <Intro />
       <Projects />
       <About />
       <Contact />
+
     </main>
   );
 }
