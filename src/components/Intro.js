6edc4712f9d2e8d51f19017e8964d9d7f5044c8e
sparsell <hello@sparsell.com>
2022-03-04 13:55:59 -0500

    added bottom margin to hero name

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,25 +1,24 @@
 import React from 'react'
-import { skills } from '../skills.js'
 
 const Intro = () => {
     return (
         <section id="Intro">
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-6xl text-5xl mb-4 font-medium text-red-50"><span>SONJA</span> PARSELL</h1>
+                <div className="lg:flex-grow lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-6xl text-5xl mb-10 font-medium text-red-50">SONJA PARSELL</h1>
 
                     <div>
                         <h3 className="title-font sm:text-3xl text-3xl mb-1 mt-5 font-small c underline underline-offset-4 text-blue-800">
                             fullstack software engineer</h3>
                             <h2 className="title-font font-semibold text-red-500 tracking-widest text-md mt-4">JavaScript | React | Redux | Ruby | Rails</h2>
 
-                            <p className="text-blue-800">I'm a Flatiron School graduate of software engineering with experience building full stack applications in modern programming lanuages and frameworks including . I continually look for ways to sharpen my skills and add new ones. I'm currently playing around with Daml and Solidity. </p>
+                            <p className="text-blue-800">I'm a Flatiron School graduate of software engineering with experience building full stack applications in modern programming lanuages and frameworks including . I continually look for ways to sharpen my skills and add new ones.  </p>
                     </div>    
 
                     <div>
                         <h3 className="title-font sm:text-3xl text-3xl mb-1 mt-5 font-small text-blue-800 underline underline-offset-4"> 
                         bent on blockchain </h3>
-                            <p className="text-blue-800">I'm captivated at what blockchain technology could do for areas of our lives that can surely stand for more equity and transparency.  I want to be a part of designing and building techonolgy that works for everyone. </p>
+                            <p className="text-blue-800">I'm captivated by what blockchain technology could do for areas of our lives that can surely stand for more equity and transparency.  I want to be a part of designing and building techonolgy that works for everyone. I'm currently playing around with Daml and Solidity.</p>
 
                         <h3 className="title-font sm:text-3xl text-3xl mb-1 mt-5 font-small text-blue-800 underline underline-offset-4"> 
                         career changer </h3>
