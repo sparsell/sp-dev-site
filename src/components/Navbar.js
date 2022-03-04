6249008f73c0f448eb74f48aa57813ebecae2c58
sparsell <hello@sparsell.com>
2022-03-04 13:55:27 -0500

    cleaned up file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,6 @@ const Navbar = () => {
         <nav class="p-2 mt-0 fixed w-full z-10 top-0 bg-white">
           <div class="container mx-auto flex flex-wrap items-center">
             <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
-                {/* <a class="text-white no-underline hover:text-white hover:no-underline" href="#">
-                    <span class="text-2xl pl-2"><i class="em em-grinning"></i> Home</span>
-                </a> */}
             </div>
             <div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
                 <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
@@ -26,33 +23,9 @@ const Navbar = () => {
                   </li>
                 </ul>
             </div>
-        </div>
-    </nav>
-        {/* <header className="md:sticky top-0 z-10">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-              <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center"></nav>
-                <a
-                href="#projects"
-                className="ml-3 bg-red-100 text-md text-blue-800 pl-1 pr-1">
-                WORK
-                </a>
-
-                <a
-                href="#about"
-                className="ml-3 bg-red-100 text-md text-blue-800 pl-1 pr-1">
-                ABOUT
-                </a>
-
-                <a
-                href="#contact"
-                className="ml-3 bg-red-100 text-md text-blue-800 pl-1 pr-1">
-                CONTACT
-                </a>
-              
-            </div>
-        </header> */}
-
-    </section>
+          </div>
+        </nav>
+      </section>
     )
 }
 
