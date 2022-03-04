89543ba375c5344a631b7d06875e92bdb96d731e
sparsell <hello@sparsell.com>
2022-03-04 12:18:05 -0500

    copy edits to project data

--- src/projectdata.js
+++ src/projectdata.js
@@ -1,7 +1,7 @@
 export const projects = [
     {   
         title: "ClassTech",
-        description: "Who in your class has a phone?", 
+        description: "Who really has a phone?", 
         image: "./project-1.gif",
         link: "https://www.myclassroomtech.com",
         github: "https://www.github.com/sparsell/classtech-frontend", 
@@ -9,24 +9,24 @@ export const projects = [
     },
     {   
         title: "SpendLess",
-        description: "Not a typical budgeting app.", 
+        description: "Not your typical budgeting app.", 
         image: "./project-2.gif",
         link: "https://www.spendless.today",
         github: "https://github.com/sparsell/spendless-frontend"
-    },
-    {   
-        title: "Quilt-a-Long",
-        description: "Hobbies are important too.", 
-        image: "./project-3.gif",
-        link: "",
-        github: "https://github.com/sparsell/quilt-a-long"
-    },
-
-    {   
-        title: "SpendLess",
-        description: "My first blockchain.", 
-        image: "./project-3.gif",
-        link: "coming soon",
-        github: "https://github.com/sparsell/classtech-frontend"
     }
+    // {   
+    //     title: "Quilt-a-Long",
+    //     description: "Hobbies are important too.", 
+    //     image: "./project-3.gif",
+    //     link: "",
+    //     github: "https://github.com/sparsell/quilt-a-long"
+    // },
+
+    // {   
+    //     title: "SpendLess",
+    //     description: "My first blockchain.", 
+    //     image: "./project-3.gif",
+    //     link: "coming soon",
+    //     github: "https://github.com/sparsell/classtech-frontend"
+    // }
 ]
