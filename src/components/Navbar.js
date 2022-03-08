a001c2b8fa0de9d8e366dcecc2ca01039ec21bc4
sparsell <hello@sparsell.com>
2022-03-08 13:46:40 -0500

    changed 'class' to 'className'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,23 @@ import React from 'react'
 const Navbar = () => {
     return (
       <section id="navbar">
-        <nav class="p-2 mt-0 fixed w-full z-10 top-0 bg-white">
-          <div class="container mx-auto flex flex-wrap items-center">
-            <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
+        <nav className="p-2 mt-0 fixed w-full z-10 top-0 bg-white">
+          <div className="container mx-auto flex flex-wrap items-center">
+            <div className="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
             </div>
-            <div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
-                <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
-                  <li class="mr-3">
-                  <a class="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#">HOME</a>
+            <div className="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
+                <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
+                  <li className="mr-3">
+                  <a className="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#intro">HOME</a>
                   </li>
-                  <li class="mr-3">
-                    <a class="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#projects">WORK</a>
+                  <li className="mr-3">
+                    <a className="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#projects">WORK</a>
                   </li>
-                  <li class="mr-3">
-                    <a class="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#about">ABOUT</a>
+                  <li className="mr-3">
+                    <a className="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#about">ABOUT</a>
                   </li>
-                    <li class="mr-3">
-                    <a class="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#contact">CONTACT</a>
+                    <li className="mr-3">
+                    <a className="inline-block text-blue-800 no-underline hover:text-red-400 hover:text-underline py-2 px-4" href="#contact">CONTACT</a>
                   </li>
                 </ul>
             </div>
