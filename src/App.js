eda053dc8be7dcd4754bca3e072769701b104546
sparsell <hello@sparsell.com>
2022-03-17 15:58:03 -0400

    adding tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './App.css'
 import Navbar from './components/Navbar'
 import Intro from './components/Intro'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import './styles/output.css'
 
 
 import './App.css';
