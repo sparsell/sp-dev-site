3311f9fd8bea959bd4b83b01918ffbdbbde23040
Sonja Parsell <sonjaparsell09@gmail.com>
2022-05-10 09:49:01 -0400

    added margin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,14 +9,14 @@ const Contact = () => {
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 mt-8 font-medium text-RED-50">C O N T A C T</h1>
                     
-                    <div className="lg:w-1/2 px-6 lg:mt-0 mb-100">
+                    <div className="lg:w-1/2 px-6 lg:mt-0 mb-100 mt-8">
                         <h2 className="title-font font-semibold text-blue-800 tracking-widest text-md mt-4">
                          EMAIL</h2>
                             <p className="text-blue-800 leading-relaxed">
                                 <a href="mailto:hello@sparsell.com">
                             hello@sparsell.com</a></p>
 
-                        <h2 className="title-font font-semibold text-blue-800 tracking-widest text-md mt-4">
+                        <h2 className="title-font font-semibold text-blue-800 tracking-widest text-md mt-8">
                             SOCIAL </h2>
                             <p className="text-blue-800 leading-relaxed"><a href="https://www.linkedin.com/in/sonja-parsell/" target="_blank" rel="noreferrer">
                                 LinkedIn</a></p>
@@ -25,7 +25,7 @@ const Contact = () => {
                             <p className="text-blue-800 leading-relaxed"><a href="https://github.com/sparsell" target="_blank" rel="noreferrer">
                                 GitHub</a></p>
 
-                        <h2 className="title-font font-semibold text-blue-800 tracking-widest text-md mt-4">
+                        <h2 className="title-font font-semibold text-blue-800 tracking-widest text-md mt-8 mb-2">
                             CV </h2> 
                          <a className="text-red-400 text-xs " href={SonjaParsellResume} target="_blank" rel="noreferrer">
                                 <button class="rounded p-1 bg-red-500 text-white" label="Resume">
