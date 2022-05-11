aff5fe5644d624895eabb0e176668f334ebb58e5
Sonja Parsell <sonjaparsell09@gmail.com>
2022-05-11 15:12:36 -0400

    added description text and a blockchain project

--- src/projectdata.js
+++ src/projectdata.js
@@ -1,8 +1,8 @@
 export const projects = [
     {   
         title: "ClassTech",
-        tagline: "Who really has a phone?", 
-        description: "",
+        tagline: "Who REALLY has a phone?", 
+        description: "My daughter was convinced everyone in her 3rd grade class had a phone. The only way I was able to prove to her this wasn't the case was to build this app. Parents can create a profile for their child and view results by classroom as to who has access to tech and who is stretching the truth.",
         tech: "",
         image: "./project-1.gif",
         link: "https://www.myclassroomtech.com",
@@ -12,25 +12,19 @@ export const projects = [
     {   
         title: "SpendLess",
         tagline: "Not your typical budgeting app.", 
-        description: "",
+        description: "Spendless is a tool to help you be more mindful of how you spend your money. We make a lot of purchases on 'automatic'. Using Spendless will help you keep track of the amounts you saved on purchases when you actually paused to consider before making them.",
         tech: "",
         image: "./project-2.gif",
         link: "https://www.spendless.today",
         github: "https://github.com/sparsell/spendless-frontend"
-    }
-    // {   
-    //     title: "Quilt-a-Long",
-    //     description: "Hobbies are important too.", 
-    //     image: "./project-3.gif",
-    //     link: "",
-    //     github: "https://github.com/sparsell/quilt-a-long"
-    // },
+    },
 
-    // {   
-    //     title: "SpendLess",
-    //     description: "My first blockchain.", 
-    //     image: "./project-3.gif",
-    //     link: "coming soon",
-    //     github: "https://github.com/sparsell/classtech-frontend"
-    // }
+    {   
+        title: "Holiday Help",
+        tagline: "A fundraiser on blockchain.",
+        description: "My first blockchain project. I'm using Solidity to make fundraising blockchain. This will be a smart contract loreum loreum ipsum loreum ipsum loreum ipsum loreum ipsum loreum ipsum loreum ipsum loreum ipsum loreum ipsum loreum ipsum loreum ipsum", 
+        image: "./project-3.gif",
+        link: "",
+        github: "https://github.com/sparsell/holiday-help"
+    }
 ]
